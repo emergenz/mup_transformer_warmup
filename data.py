fcad85518eeb76c89b1564f7f33b6ed7ad7fcebd
Franz Srambical <franz.srambical@gmail.com>
2024-07-24 12:53:03 +0200

    feat: use GPT2Tokenizer

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 import os
 from io import open
 import torch
+from transformers import GPT2Tokenizer
 
 class Dictionary(object):
     def __init__(self):
@@ -16,10 +17,9 @@ class Dictionary(object):
     def __len__(self):
         return len(self.idx2word)
 
-
 class Corpus(object):
     def __init__(self, path):
-        self.dictionary = Dictionary()
+        self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
         self.train = None
         self.valid = None
         self.test = None
@@ -30,41 +30,26 @@ class Corpus(object):
             self.save_cache(path)
 
     def load_cache(self, path):
-        for cache in ['dict.pt', 'train.pt', 'valid.pt', 'test.pt']:
+        for cache in ['tokenizer.pt', 'train.pt', 'valid.pt', 'test.pt']:
             cache_path = os.path.join(path, cache)
             if not os.path.exists(cache_path):
                 return False
-        self.dictionary = torch.load(os.path.join(path, 'dict.pt'))
+        self.tokenizer = torch.load(os.path.join(path, 'tokenizer.pt'))
         self.train = torch.load(os.path.join(path, 'train.pt'))
         self.valid = torch.load(os.path.join(path, 'valid.pt'))
         self.test = torch.load(os.path.join(path, 'test.pt'))
         return True
 
     def save_cache(self, path):
-        torch.save(self.dictionary, os.path.join(path, 'dict.pt'))
+        torch.save(self.tokenizer, os.path.join(path, 'tokenizer.pt'))
         torch.save(self.train, os.path.join(path, 'train.pt'))
         torch.save(self.valid, os.path.join(path, 'valid.pt'))
         torch.save(self.test, os.path.join(path, 'test.pt'))
-    
+
     def tokenize(self, path):
-        """Tokenizes a text file."""
+        """Tokenizes a text file using GPT2Tokenizer."""
         assert os.path.exists(path)
-        # Add words to the dictionary
         with open(path, 'r', encoding="utf8") as f:
-            for line in f:
-                words = line.split() + ['<eos>']
-                for word in words:
-                    self.dictionary.add_word(word)
-
-        # Tokenize file content
-        with open(path, 'r', encoding="utf8") as f:
-            idss = []
-            for line in f:
-                words = line.split() + ['<eos>']
-                ids = []
-                for word in words:
-                    ids.append(self.dictionary.word2idx[word])
-                idss.append(torch.tensor(ids).type(torch.int64))
-            ids = torch.cat(idss)
-
-        return ids
+            text = f.read()
+        tokens = self.tokenizer.encode(text, add_special_tokens=True)
+        return torch.tensor(tokens).type(torch.int64)
