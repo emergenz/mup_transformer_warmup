95b423079f7bcb195b9c907fe905593c878e58b6
Franz Srambical <franz.srambical@gmail.com>
2024-07-24 13:08:59 +0200

    feat: remove custom dictionary class

--- data.py
+++ data.py
@@ -3,20 +3,6 @@ from io import open
 import torch
 from transformers import GPT2Tokenizer
 
-class Dictionary(object):
-    def __init__(self):
-        self.word2idx = {}
-        self.idx2word = []
-
-    def add_word(self, word):
-        if word not in self.word2idx:
-            self.idx2word.append(word)
-            self.word2idx[word] = len(self.idx2word) - 1
-        return self.word2idx[word]
-
-    def __len__(self):
-        return len(self.idx2word)
-
 class Corpus(object):
     def __init__(self, path):
         self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
