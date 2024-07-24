8252a499e96cf9a3defc1bac57e66f70b4b1db93
Franz Srambical <franz.srambical@gmail.com>
2024-07-24 13:42:01 +0200

    fix: order of batchify on subset

--- main.py
+++ main.py
@@ -46,13 +46,13 @@ def batchloader(train_data, bptt):
         yield get_batch(train_data, i, bptt)
 
 def batchify(data, bsz, device, data_subset_ratio):
+    # Use only the specified percentage of the data
+    data_len = int(data.size(0) * data_subset_ratio)
+    data = data[:data_len]
     # Work out how cleanly we can divide the dataset into bsz parts.
     nbatch = data.size(0) // bsz
     # Trim off any extra elements that wouldn't cleanly fit (remainders).
     data = data.narrow(0, 0, nbatch * bsz)
-    # Use only the specified percentage of the data
-    data_len = int(data.size(0) * data_subset_ratio)
-    data = data[:data_len]
     # Evenly divide the data across the bsz batches.
     data = data.view(bsz, -1).t().contiguous()
     return data.to(device)
